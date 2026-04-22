1e6bf27b597a893e399e77f0c711e51d09cd26d4
Wenura1 <wenuramgnss@gmail.com>
2026-04-23 01:10:17 +0530

    Updated the AppConfig.java

--- src/main/java/com/smartcampus/config/AppConfig.java
+++ src/main/java/com/smartcampus/config/AppConfig.java
@@ -3,7 +3,9 @@ package com.smartcampus.config;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+// this sets the base URL for all endpoints
+// everything will be under /api/v1
 @ApplicationPath("/api/v1")
 public class AppConfig extends Application {
-    // base path config
+    // no need to write anything inside
 }
